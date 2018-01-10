065c3f255feae299d1f885363e928d26d51264a1
olivcamj <34360644+olivcamj@users.noreply.github.com>
2018-01-10 10:32:44 -0500

    switch case ex

---
+++ SwitchExmple.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchExmple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter day number (1 for Mon -7 for Sun)");
+           int daynumber = int.Parse(Console.ReadLine());
+            //daynumber=5
+
+            switch (daynumber)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Mon");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Tues");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Wedn");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Thur");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Fri");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Sat");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Sun");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid Input.Try Again.");
+                    }
+                    
+            }
+            Console.ReadLine();
+        }
+    }
+}
